b9f88b68b3451da281257dedac9076a5f7edd00f
Lechinoix <nicolas.ngomai@gmail.com>
2017-07-12 02:20:36 +0200

    Implement Video

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
 import Header from './header';
+import ModalVideo from './ModalVideo';
 import Home from 'src/routes/home';
 import Meetups from 'src/routes/meetups';
 
@@ -9,6 +10,14 @@ import Meetups from 'src/routes/meetups';
 // import Profile from 'async!./profile';
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isModalOpened: false,
+			pictureUrl: '',
+		};
+	}
+
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
@@ -17,14 +26,38 @@ export default class App extends Component {
 		this.currentUrl = e.url;
 	};
 
+	openModalVideo = () => {
+		this.setState({ isModalOpened: true });
+	};
+
+	onTakePicture = (pictureUrl) => {
+		this.setState({
+			isModalOpened: false,
+			pictureUrl,
+		});
+	}
+
 	render() {
+		const appStyle = {
+			width: '100vw',
+			height: '100vh',
+			overflow: 'hidden',
+		}
 		return (
-			<div id="app">
+			<div id="app" style={appStyle}>
 			<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 			<link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css" />
+				<ModalVideo
+					isOpened={this.state.isModalOpened}
+					onTakePicture={this.onTakePicture}
+				/>
 				<Header />
 				<Router onChange={this.handleRoute}>
-					<Home path="/map/" />
+					<Home
+						openModalVideo={this.openModalVideo}
+						pictureUrl={this.state.pictureUrl}
+						path="/map/"
+					/>
 					<Meetups path="/meetups/" />
 				</Router>
 			</div>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,51 +1,36 @@
 import { h, Component } from 'preact';
-import style from './style';
 import Maps from 'src/components/maps';
-import { Button } from 'preact-mdl';
+import { Button, Icon } from 'preact-mdl';
 
 export default class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			picture: null,
-		}
-	}
-
-	gotMedia = (mediaStream) => {
-		const mediaStreamTrack = mediaStream.getVideoTracks()[0];
-		const imageCapture = new ImageCapture(mediaStreamTrack);
-		imageCapture.takePhoto()
-	  .then(blob => {
-	    this.setState({ picture: URL.createObjectURL(blob)});
-	    img.onload = () => { URL.revokeObjectURL(this.src); }
-	  })
-	  .catch(error => console.error('takePhoto() error:', error));
-	}
-
-	takePicture = () => {
-		navigator.mediaDevices.getUserMedia({video: true})
-		  .then(this.gotMedia)
-		  .catch(error => console.error('getUserMedia() error:', error));
-	}
-
 	render() {
+		const styles = {
+			container: {
+				padding: '56px 0',
+				minHeight: '100%',
+				width: '100%',
+			},
+			button: {
+				position: 'fixed',
+				bottom: '20px',
+				right: '20px',
+			},
+		}
 		return (
-			<div class={style.home}>
+			<div style={styles.container}>
+				{this.props.pictureUrl ?
+					<img src={this.props.pictureUrl} /> :
+					null}
 				<Maps />
-				<div class={style.picture}>
-					<img
-						src={this.state.picture}
-						onLoad={() => { URL.revokeObjectURL(this.state.picture); }}
-					/>
-				</div>
-				<div class={style.addButton}>
-					<Button
-						onClick={this.takePicture}
-						fab
-						raised
-						colored
-					>+</Button>
-				</div>
+				<Button
+					onClick={this.props.openModalVideo}
+					fab
+					raised
+					colored
+					style={styles.button}
+				>
+					<Icon icon="add" />
+				</Button>
 			</div>
 		);
 	}
